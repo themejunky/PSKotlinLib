d04732ffc11b3b1e9ba42d5898e33e35f8c59afa
Bejan Corneliu <bejancorneliu@gmail.com>
2018-08-26 14:50:24 +0300

    - custom button finish

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customButton/CustomButton.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customButton/CustomButton.kt
@@ -7,34 +7,79 @@ import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.tools.use
 import kotlinx.android.synthetic.main.custom_button.view.*
 import android.graphics.drawable.GradientDrawable
 import android.support.v4.content.ContextCompat
+import android.view.View
 
 class CustomButton(context: Context, attrs: AttributeSet) : CustomButtonBase(context, attrs) {
+
+    /* click listener interface */
+    interface Custom_Button_Interface {
+        fun onCustomButtonClick(view: View)
+    }
+
+    fun setListener(nListener : Custom_Button_Interface) {
+        mListener = nListener
+    }
 
     init {
         inflate(context, R.layout.custom_button, this)
         context.obtainStyledAttributes(attrs, R.styleable.CustomButton).use {
 
-
             mInactiveBackgroundDrawable = (ContextCompat.getDrawable(context, R.drawable.shadow2) as GradientDrawable)
-            mInactiveBackgroundDrawable.setColor(ContextCompat.getColor(context,R.color.lib_button_inactive_background))
+            mInactiveBackgroundDrawable.setColor(ContextCompat.getColor(context, R.color.lib_button_inactive_background))
 
             mInactiveShadowDrawable = (ContextCompat.getDrawable(context, R.drawable.shadow2) as GradientDrawable)
-            mInactiveShadowDrawable.setColor(ContextCompat.getColor(context,R.color.lib_button_inactive_shadow))
+            mInactiveShadowDrawable.setColor(ContextCompat.getColor(context, R.color.lib_button_inactive_shadow))
+
+            if (it.hasValue(R.styleable.CustomButton_cb_text)) {
+                nButton.text = it.getString(R.styleable.CustomButton_cb_text)
+            }
+
+            if (it.hasValue(R.styleable.CustomButton_cb_tag)) {
+                nContainer.tag = it.getString(R.styleable.CustomButton_cb_tag)
+            }
 
             if (it.hasValue(R.styleable.CustomButton_cb_active_background)) {
                 mActiveBackgroundDrawable = (ContextCompat.getDrawable(context, R.drawable.shadow2) as GradientDrawable)
-                mActiveBackgroundDrawable.setColor(it.getColor(R.styleable.CustomButton_cb_active_background,0))
+                mActiveBackgroundDrawable.setColor(it.getColor(R.styleable.CustomButton_cb_active_background, 0))
             }
 
             if (it.hasValue(R.styleable.CustomButton_cb_active_shadow)) {
                 mActiveShadowDrawable = (ContextCompat.getDrawable(context, R.drawable.shadow2) as GradientDrawable)
-                mActiveShadowDrawable.setColor(it.getColor(R.styleable.CustomButton_cb_active_shadow,0))
+                mActiveShadowDrawable.setColor(it.getColor(R.styleable.CustomButton_cb_active_shadow, 0))
             }
 
-            nButton.background = mActiveBackgroundDrawable
-            nShadow.background = mActiveShadowDrawable
-
+            if (it.hasValue(R.styleable.CustomButton_cb_form_validation)) {
+                if (it.getBoolean(R.styleable.CustomButton_cb_form_validation, false)) {
+                    mInactiveRelease()
+                } else {
+                    mActiveRelease()
+                }
+            }
             nContainer.setOnTouchListener(this)
         }
+    }
+
+    fun setTag(nTag: Int) {
+        nContainer.tag = context.getString(nTag)
+    }
+
+    fun setTag(nTag: String) {
+        nContainer.tag = nTag
+    }
+
+    fun setText(nText: Int) {
+        nButton.text = context.getString(nText)
+    }
+
+    fun setText(nText: String) {
+        nButton.text = nText
+    }
+
+    fun setActive() {
+        mActiveRelease()
+    }
+
+    fun setInactive() {
+        mInactiveRelease()
     }
 }
--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customButton/CustomButtonBase.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customButton/CustomButtonBase.kt
@@ -2,17 +2,14 @@ package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.cu
 
 import android.content.Context
 import android.graphics.drawable.GradientDrawable
-import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.custom_button.view.*
-import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.R
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.BaseCustomView
 
 const val ACTIVE_BUTTON = "active"
 const val INACTIVE_BUTTON = "inactive"
-
 
 open class CustomButtonBase(context: Context, attrs: AttributeSet) : BaseCustomView(context, attrs), View.OnTouchListener {
 
@@ -20,24 +17,26 @@ open class CustomButtonBase(context: Context, attrs: AttributeSet) : BaseCustomV
     var mActiveShadowDrawable : GradientDrawable = GradientDrawable()
     var mInactiveBackgroundDrawable : GradientDrawable = GradientDrawable()
     var mInactiveShadowDrawable : GradientDrawable = GradientDrawable()
+    lateinit var mListener : CustomButton.Custom_Button_Interface
 
     override fun onTouch(p0: View, p1: MotionEvent): Boolean {
-        if (p1.action==MotionEvent.ACTION_DOWN) {
-            if (this.tag!=null) {
-                when(this.tag) {
-                    ACTIVE_BUTTON -> mActiveTouch()
-                    INACTIVE_BUTTON -> mInactiveTouch()
+
+        if (nContainer.tag!=null) {
+            nContainer.tag.let {
+                if (p1.action==MotionEvent.ACTION_DOWN) {
+                    when(it) {
+                        ACTIVE_BUTTON -> mActiveTouch()
+                        INACTIVE_BUTTON -> mInactiveTouch()
+                    }
+                    return true
                 }
-            }
-        } else if (p1.action==MotionEvent.ACTION_UP) {
-            if (this.tag!=null) {
-                this.tag.let {
+                else if (p1.action==MotionEvent.ACTION_UP) {
                     when(it) {
                         ACTIVE_BUTTON -> mActiveRelease()
                         INACTIVE_BUTTON -> mInactiveRelease()
                     }
+                    return true
                 }
-
             }
         }
         return false
@@ -49,7 +48,7 @@ open class CustomButtonBase(context: Context, attrs: AttributeSet) : BaseCustomV
         nShadow.visibility=View.GONE
     }
 
-    private fun mActiveRelease() {
+    protected fun mActiveRelease() {
         nButton.background = mActiveBackgroundDrawable;
         nShadow.background = mActiveShadowDrawable
         nShadow.visibility=View.VISIBLE
@@ -61,10 +60,9 @@ open class CustomButtonBase(context: Context, attrs: AttributeSet) : BaseCustomV
         nShadow.visibility=View.GONE
     }
 
-    private fun mInactiveRelease() {
+    protected fun mInactiveRelease() {
         nButton.background = mInactiveBackgroundDrawable;
         nShadow.background = mInactiveShadowDrawable
         nShadow.visibility=View.VISIBLE
     }
-
 }
