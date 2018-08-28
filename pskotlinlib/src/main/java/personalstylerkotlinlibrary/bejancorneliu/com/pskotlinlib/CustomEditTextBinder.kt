2fdeb0f117ecdd38f9b4b46040de828d6502b074
Bejan Corneliu <bejancorneliu@gmail.com>
2018-08-28 23:05:42 +0300

    - CustomInput(31)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/CustomEditTextBinder.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/CustomEditTextBinder.kt
@@ -13,8 +13,7 @@ import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.cus
 class CustomEditTextBinder {
     companion object {
         @JvmStatic
-      //  @BindingAdapter(value = ["textAttrChanged"])
-        @InverseBindingAdapter(attribute = "android:Text", event = "android:textAttrChanged")
+        @BindingAdapter(value = ["textAttrChanged"])
         fun setListener(editText: CustomInput, listener: InverseBindingListener?) {
             if (listener != null) {
                 editText.nInput.addTextChangedListener(object : TextWatcher {
