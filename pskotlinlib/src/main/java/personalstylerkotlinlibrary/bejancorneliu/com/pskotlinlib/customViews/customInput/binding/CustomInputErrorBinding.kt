488b1db2850fc0aee42de19ac0a674667b10ae58
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-14 14:51:09 +0300

    - CustomInput(10)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputErrorBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputErrorBinding.kt
@@ -8,15 +8,15 @@ import kotlinx.android.synthetic.main.lib_custom_input.view.*
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.CustomInput
 
 
-@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "ci_error_text"))
-class CustomInputErrorBinding {
-    companion object {
-        @JvmStatic
-        @BindingAdapter("ci_error_text")
-        fun setci_error_text(editText: CustomInput, text: String) {
-            editText.nInputError.text = text
-            editText.nInputMandatory.visibility = View.VISIBLE
-            editText.mDefaultError = text
-        }
-    }
-}
+//@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "ci_error_text"))
+//class CustomInputErrorBinding {
+//    companion object {
+//        @JvmStatic
+//        @BindingAdapter("ci_error_text")
+//        fun setci_error_text(editText: CustomInput, text: String) {
+//            editText.nInputError.text = text
+//            editText.nInputMandatory.visibility = View.VISIBLE
+//            editText.mDefaultError = text
+//        }
+//    }
+//}
