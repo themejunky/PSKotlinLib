2882a8e8f28a2f9def923a60b62491b24fa53073
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-14 13:56:25 +0300

    - CustomInput(6)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputErrorBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputErrorBinding.kt
@@ -3,6 +3,7 @@ package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.cu
 import android.databinding.BindingAdapter
 import android.databinding.InverseBindingMethod
 import android.databinding.InverseBindingMethods
+import android.view.View
 import kotlinx.android.synthetic.main.lib_custom_input.view.*
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.CustomInput
 
@@ -14,6 +15,8 @@ class CustomInputErrorBinding {
         @BindingAdapter("ci_error_text")
         fun setci_error_text(editText: CustomInput, text: String) {
             editText.nInputError.text = text
+            editText.nInputMandatory.visibility = View.VISIBLE
+            editText.mDefaultError = text
         }
     }
 }
