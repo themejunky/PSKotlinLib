2111865c02d63441f1df13e82987431cc31e92e4
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-14 15:00:40 +0300

    - CustomInput(11)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputShowErrorBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputShowErrorBinding.kt
@@ -5,13 +5,13 @@ import android.databinding.InverseBindingMethod
 import android.databinding.InverseBindingMethods
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.CustomInput
 
-@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "ci_show_error"))
-class CustomInputShowErrorBinding {
-    companion object {
-        @JvmStatic
-        @BindingAdapter("ci_show_error")
-        fun setci_show_error(editText: CustomInput, text: Boolean) {
-            editText.mIsInputValid = text
-        }
-    }
-}
+//@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "ci_show_error"))
+//class CustomInputShowErrorBinding {
+//    companion object {
+//        @JvmStatic
+//        @BindingAdapter("ci_show_error")
+//        fun setci_show_error(editText: CustomInput, text: Boolean) {
+//            editText.mIsInputValid = text
+//        }
+//    }
+//}
