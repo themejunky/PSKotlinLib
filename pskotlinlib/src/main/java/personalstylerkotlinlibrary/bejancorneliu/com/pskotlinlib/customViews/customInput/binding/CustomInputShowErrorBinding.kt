a2ffaa6ef01ad1708c63f4108c1245af7c575b29
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-14 16:11:15 +0300

    - CustomInput(13)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputShowErrorBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputShowErrorBinding.kt
@@ -3,15 +3,19 @@ package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.cu
 import android.databinding.BindingAdapter
 import android.databinding.InverseBindingMethod
 import android.databinding.InverseBindingMethods
+import android.util.Log
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.CustomInput
 
-//@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "ci_show_error"))
-//class CustomInputShowErrorBinding {
-//    companion object {
-//        @JvmStatic
-//        @BindingAdapter("ci_show_error")
-//        fun setci_show_error(editText: CustomInput, text: Boolean) {
-//            editText.mIsInputValid = text
-//        }
-//    }
-//}
+@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "ci_show_error"))
+class CustomInputShowErrorBinding {
+    companion object {
+        @JvmStatic
+        @BindingAdapter("ci_show_error")
+        fun setci_show_error(editText: CustomInput, text: Boolean) {
+            if (text) {
+                editText.triggerError() }
+            else {
+                editText.triggerDefault() }
+        }
+    }
+}
