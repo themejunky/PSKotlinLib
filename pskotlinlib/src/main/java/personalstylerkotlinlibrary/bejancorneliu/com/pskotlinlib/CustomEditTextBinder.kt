059357e97ecf210fb9c699acfc3887238e5131a1
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-14 12:31:42 +0300

    - CustomInput(3)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/CustomEditTextBinder.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/CustomEditTextBinder.kt
@@ -10,69 +10,69 @@ import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.cus
 import android.databinding.InverseBindingAdapter
 
 
-//
-////,event = "android:textAttrChanged",method = "getText"
-//@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "textulmeu"))
-//class CustomEditTextBinder {
-//    companion object {
-//        @JvmStatic
-//        @BindingAdapter(value = ["textulmeuAttrChanged"], requireAll = false)
-//        fun setListener(editText: CustomInput, listener: InverseBindingListener?) {
-//            if (listener != null) {
-//                editText.nInput.addTextChangedListener(object : TextWatcher {
-//                    override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-//
-//                    }
-//
-//                    override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-//
-//                    }
-//
-//                    override fun afterTextChanged(editable: Editable) {
-//                        listener.onChange()
-//                    }
-//                })
-//            }
+
+//,event = "android:textAttrChanged",method = "getText"
+@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "textulmeu"))
+class CustomEditTextBinder {
+    companion object {
+        @JvmStatic
+        @BindingAdapter(value = ["textulmeuAttrChanged"], requireAll = false)
+        fun setListener(editText: CustomInput, listener: InverseBindingListener?) {
+            if (listener != null) {
+                editText.nInput.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+
+                    }
+
+                    override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+
+                    }
+
+                    override fun afterTextChanged(editable: Editable) {
+                        listener.onChange()
+                    }
+                })
+            }
+        }
+
+//        @JvmStatic @InverseBindingAdapter(attribute = "android:text", event = "android:textAttrChanged")
+//        fun getTextString(editText: CustomInput): String {
+//            return editText.nInput.text.toString()
 //        }
-//
-////        @JvmStatic @InverseBindingAdapter(attribute = "android:text", event = "android:textAttrChanged")
-////        fun getTextString(editText: CustomInput): String {
-////            return editText.nInput.text.toString()
-////        }
-//
-//
-//
-//
-//        @JvmStatic
-//        @InverseBindingAdapter(attribute = "textulmeu")
-//        fun getTextulmeu(nMe: CustomInput): String {
-//            return nMe.nInput.text.toString()
-//        }
-//
-//        @JvmStatic
-//        @BindingAdapter("textulmeu")
-//        fun setTextulmeu(editText: CustomInput, text: String?) {
-//            text?.let {
-//                if (it != editText.nInput.text.toString()) {
-//                    editText.nInput.setText(it)
-//                }
-//            }
-//        }
-//
-//        @JvmStatic
-//        @InverseBindingAdapter(attribute = "textulmeu")
-//        fun gettextulmeu(nMe: CustomInput): String {
-//            return nMe.nInput.text.toString()
-//        }
-//
-//        @JvmStatic
-//        @BindingAdapter("textulmeu")
-//        fun settextulmeu(editText: CustomInput, text: String?) {
-//            text?.let {
-//                if (it != editText.nInput.text.toString()) {
-//                    editText.nInput.setText(it)
-//                }
-//            }
-//        }
-//    }
-//}
+
+
+
+
+        @JvmStatic
+        @InverseBindingAdapter(attribute = "textulmeu")
+        fun getTextulmeu(nMe: CustomInput): String {
+            return nMe.nInput.text.toString()
+        }
+
+        @JvmStatic
+        @BindingAdapter("textulmeu")
+        fun setTextulmeu(editText: CustomInput, text: String?) {
+            text?.let {
+                if (it != editText.nInput.text.toString()) {
+                    editText.nInput.setText(it)
+                }
+            }
+        }
+
+        @JvmStatic
+        @InverseBindingAdapter(attribute = "textulmeu")
+        fun gettextulmeu(nMe: CustomInput): String {
+            return nMe.nInput.text.toString()
+        }
+
+        @JvmStatic
+        @BindingAdapter("textulmeu")
+        fun settextulmeu(editText: CustomInput, text: String?) {
+            text?.let {
+                if (it != editText.nInput.text.toString()) {
+                    editText.nInput.setText(it)
+                }
+            }
+        }
+    }
+}
---
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/CustomInputBinding.kt
@@ -0,0 +1,47 @@
+package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput
+
+import android.databinding.*
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.lib_custom_input.view.*
+
+@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class,attribute = "ci_text_text"))
+class CustomInputBinding {
+    companion object {
+        @JvmStatic
+        @BindingAdapter("ci_text_text")
+        fun setci_text_text(editText: CustomInput, text: String?) {
+            text?.let {
+                if (it != editText.nInput.text.toString()) {
+                    editText.nInput.setText(it)
+                }
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter(value = ["ci_text_textAttrChanged"], requireAll = false)
+        fun setListener(editText: CustomInput, listener: InverseBindingListener?) {
+            if (listener != null) {
+                editText.nInput.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+
+                    }
+
+                    override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+
+                    }
+
+                    override fun afterTextChanged(editable: Editable) {
+                        listener.onChange()
+                    }
+                })
+            }
+        }
+
+        @JvmStatic
+        @InverseBindingAdapter(attribute = "ci_text_text")
+        fun getci_text_text(nMe: CustomInput): String {
+            return nMe.nInput.text.toString()
+        }
+    }
+}
