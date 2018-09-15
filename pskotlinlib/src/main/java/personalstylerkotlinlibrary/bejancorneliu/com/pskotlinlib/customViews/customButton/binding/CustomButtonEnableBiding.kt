b18567f16617562d407a80cbe9c41f22562b9276
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-15 18:14:51 +0300

    - CustomButton(2)

---
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customButton/binding/CustomButtonEnableBiding.kt
@@ -0,0 +1,22 @@
+package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customButton.binding
+
+import android.databinding.BindingAdapter
+import android.databinding.InverseBindingMethod
+import android.databinding.InverseBindingMethods
+import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customButton.CustomButton
+
+@InverseBindingMethods(InverseBindingMethod(type = CustomButton::class,attribute = "cb_enable"))
+class CustomButtonEnableBiding {
+    companion object {
+        @JvmStatic
+        @BindingAdapter("cb_enable")
+        fun setcb_enable(mCustomButton: CustomButton, mEnable: Boolean) {
+            if (mEnable) {
+                mCustomButton.setActive()
+            } else {
+                mCustomButton.setInactive()
+            }
+
+        }
+    }
+}
