7e2e80a285ce72e8fe22eb80dda991eb9d1a036c
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-16 22:58:30 +0300

    - CustomBoxImage(8)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImage/binding/CustomBoxImageSelectedBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImage/binding/CustomBoxImageSelectedBinding.kt
@@ -1,7 +1,23 @@
 package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customBoxImage.binding
 
 import android.databinding.BindingAdapter
+import android.databinding.InverseBindingMethod
+import android.databinding.InverseBindingMethods
 import android.widget.CompoundButton
+import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customBoxImage.CustomBoxImage
+import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.CustomInput
+
+
+@InverseBindingMethods(InverseBindingMethod(type = CustomInput::class, attribute = "cbi_is_selected"))
+class CustomBoxImageSelectedBinding {
+    companion object {
+        @JvmStatic
+        @BindingAdapter("cbi_is_selected")
+        fun setcbi_is_selected(editText: CustomBoxImage, text: Boolean) {
+            editText.setActivationState(text)
+        }
+    }
+}
 
 @BindingAdapter("android:checked")
 fun setChecked(checkableView: CompoundButton, isChecked: Boolean?) {
