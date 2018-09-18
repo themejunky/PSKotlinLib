5c9121d0330d5924696608aa804b091e93bebd92
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-18 11:57:28 +0300

    - DataBase(1)

---
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/databases/Cities.kt
@@ -0,0 +1,19 @@
+package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.databases
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "cities")
+data class Cities(
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "uid")
+        var uid: Int = 0,
+
+        @ColumnInfo(name = "id")
+        var id: String = "",
+
+        @ColumnInfo(name = "name")
+        var name: String = ""
+
+)
