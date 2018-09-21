de1a31eacd5c948fc6d4f2ea53dd58778e66fffc
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-21 13:58:42 +0300

    - CustomSpinner(8)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customSpinner/binding/CustomSpinnerPostionBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customSpinner/binding/CustomSpinnerPostionBinding.kt
@@ -4,42 +4,42 @@ import android.databinding.*
 import android.view.View
 import android.widget.AdapterView
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customSpinner.CustomSpinner
-
-@InverseBindingMethods(InverseBindingMethod(type = CustomSpinner::class,attribute = "cs_position"))
-class CustomSpinnerPostionBinding {
-    companion object {
-
-        @JvmStatic
-        @BindingAdapter("cs_position")
-        fun setcs_positions(editText: CustomSpinner, entries: Int?) {
-            if (entries!=null) {
-                editText.getSpinner().setSelection(entries)
-            }
-        }
-
-        @JvmStatic
-        @BindingAdapter(value = ["cs_positionAttrChanged"], requireAll = false)
-        fun setProvinceListener(view: CustomSpinner?, cityChange: InverseBindingListener?) {
-            if (view!=null && cityChange!=null) {
-                val listener = object : AdapterView.OnItemSelectedListener {
-                    override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                        cityChange.onChange()
-                    }
-
-                    override fun onNothingSelected(parent: AdapterView<*>) {
-                        cityChange.onChange()
-                    }
-                }
-                if (view.getSpinner()!=null && listener!=null)
-                view.getSpinner().onItemSelectedListener = listener
-            }
-        }
-
-
-        @JvmStatic
-        @InverseBindingAdapter(attribute = "cs_position")
-        fun getcs_position(nMe: CustomSpinner): Int {
-            return nMe.getSpinner().selectedItemPosition
-        }
-    }
-}
+//
+//@InverseBindingMethods(InverseBindingMethod(type = CustomSpinner::class,attribute = "cs_position"))
+//class CustomSpinnerPostionBinding {
+//    companion object {
+//
+//        @JvmStatic
+//        @BindingAdapter("cs_position")
+//        fun setcs_positions(editText: CustomSpinner, entries: Int?) {
+//            if (entries!=null) {
+//                editText.getSpinner().setSelection(entries)
+//            }
+//        }
+//
+//        @JvmStatic
+//        @BindingAdapter(value = ["cs_positionAttrChanged"], requireAll = false)
+//        fun setProvinceListener(view: CustomSpinner?, cityChange: InverseBindingListener?) {
+//            if (view!=null && cityChange!=null) {
+//                val listener = object : AdapterView.OnItemSelectedListener {
+//                    override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+//                        cityChange.onChange()
+//                    }
+//
+//                    override fun onNothingSelected(parent: AdapterView<*>) {
+//                        cityChange.onChange()
+//                    }
+//                }
+//                if (view.getSpinner()!=null && listener!=null)
+//                    view.getSpinner().onItemSelectedListener = listener
+//            }
+//        }
+//
+//
+//        @JvmStatic
+//        @InverseBindingAdapter(attribute = "cs_position")
+//        fun getcs_position(nMe: CustomSpinner): Int {
+//            return nMe.getSpinner().selectedItemPosition
+//        }
+//    }
+//}
