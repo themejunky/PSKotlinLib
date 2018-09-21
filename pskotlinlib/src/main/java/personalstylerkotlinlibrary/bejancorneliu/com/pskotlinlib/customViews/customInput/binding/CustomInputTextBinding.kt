2a9a24c356130cc6830bf7448db45eed1b07e290
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-21 21:07:41 +0300

    - CustomButton(3)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputTextBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputTextBinding.kt
@@ -29,7 +29,7 @@ class CustomInputTextBinding {
                     }
 
                     override fun afterTextChanged(editable: Editable) {
-                        nListener.onChange()
+                        // nListener.onChange()
                     }
                 })
             }
