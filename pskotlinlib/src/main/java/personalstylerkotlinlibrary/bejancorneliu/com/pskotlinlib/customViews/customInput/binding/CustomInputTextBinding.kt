7f978e37af14a9c4355b212352cf5b8bdd85ddd2
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-21 23:26:34 +0300

    - CustomButton(9)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputTextBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customInput/binding/CustomInputTextBinding.kt
@@ -1,8 +1,8 @@
 package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.binding
 
-import android.databinding.BindingAdapter
-import android.databinding.InverseBindingMethod
-import android.databinding.InverseBindingMethods
+import android.databinding.*
+import android.text.Editable
+import android.text.TextWatcher
 import kotlinx.android.synthetic.main.lib_custom_input.view.*
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.CustomInput
 
@@ -11,46 +11,36 @@ class CustomInputTextBinding {
     companion object {
         @JvmStatic
         @BindingAdapter("ci_text_text")
-        fun setci_text_text(nCustomInput: CustomInput?, nValue: String?) {
-            if (nCustomInput != null && nValue != null) {
-                nCustomInput.nInput.setText(nValue)
+        fun setci_text_text(editText: CustomInput, text: String?) {
+            text?.let {
+                if (it != editText.nInput.text.toString()) {
+                    editText.nInput.setText(it)
+                }
             }
         }
-//
-//        @JvmStatic
-//        @BindingAdapter(value = ["ci_text_textAttrChanged"], requireAll = false)
-//        fun setListener(nCustomInput: CustomInput?, nListener: InverseBindingListener?) {
-//            if (nListener != null && nCustomInput != null) {
-//                var mCompare1: String = ""
-//                var mCompare2: String = ""
-//                nCustomInput.nInput.addTextChangedListener(object : TextWatcher {
-//                    override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-//                        nCustomInput.nInput.setSelection(nCustomInput.nInput.length())
-//                        mCompare1 = charSequence.toString()
-//                    }
-//
-//                    override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-//                        nCustomInput.nInput.setSelection(nCustomInput.nInput.length())
-//                        mCompare2 = charSequence.toString()
-//                    }
-//
-//                    override fun afterTextChanged(editable: Editable) {
-//                        if (mCompare1 != mCompare2) {
-//                            nListener.onChange()
-//                        }
-//                    }
-//                })
-//            }
-//        }
-//
-//        @JvmStatic
-//        @InverseBindingAdapter(attribute = "ci_text_text")
-//        fun getci_text_text(nCustomInput: CustomInput?): String {
-//            if (nCustomInput != null) {
-//                return nCustomInput.nInput.text.toString()
-//            } else {
-//                return "null"
-//            }
-//        }
+
+        @JvmStatic
+        @BindingAdapter(value = ["ci_text_textAttrChanged"], requireAll = false)
+        fun setListener(editText: CustomInput, listener: InverseBindingListener?) {
+            if (listener != null) {
+                editText.nInput.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                    }
+
+                    override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                    }
+
+                    override fun afterTextChanged(editable: Editable) {
+                        listener.onChange()
+                    }
+                })
+            }
+        }
+
+        @JvmStatic
+        @InverseBindingAdapter(attribute = "ci_text_text")
+        fun getci_text_text(nMe: CustomInput): String {
+            return nMe.nInput.text.toString()
+        }
     }
 }
