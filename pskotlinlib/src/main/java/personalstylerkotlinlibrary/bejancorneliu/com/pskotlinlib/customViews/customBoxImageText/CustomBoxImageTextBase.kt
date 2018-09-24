4e34093c0c2b18ddad45ef3644e2daf3e9296725
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-24 21:10:27 +0300

    - CustomBoxImageText(6)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImageText/CustomBoxImageTextBase.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImageText/CustomBoxImageTextBase.kt
@@ -32,6 +32,7 @@ abstract class CustomBoxImageTextBase(context: Context, attrs: AttributeSet) : B
                     mText.setTextColor(mActiveTextColor)
                     mImage.setColorFilter(mActiveTextColor)
                     mContainer.background = ContextCompat.getDrawable(context, R.drawable.lib_container_round_corners_active)
+                    mContainer.background = ContextCompat.getDrawable(context, R.drawable.lib_container_round_corners)
                 } else {
                     mText.setTextColor(ContextCompat.getColor(context, R.color.lib_base_black))
                     mImage.setColorFilter(ContextCompat.getColor(context, R.color.lib_base_black))
@@ -59,6 +60,7 @@ abstract class CustomBoxImageTextBase(context: Context, attrs: AttributeSet) : B
             mText.setTextColor(mActiveTextColor)
             mImage.setColorFilter(mActiveTextColor)
             mContainer.background = ContextCompat.getDrawable(context, R.drawable.lib_container_round_corners_active)
+            mContainer.background = ContextCompat.getDrawable(context, R.drawable.lib_container_round_corners)
         } else {
             mText.setTextColor(ContextCompat.getColor(context, R.color.lib_base_black))
             mImage.setColorFilter(ContextCompat.getColor(context, R.color.lib_base_black))
