ea4707854b0fb2346a307b5649afe05dc86158f7
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-27 14:58:54 +0300

    - CustomImageGallery(10)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImageText/binding/CustomBoxImageTextBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImageText/binding/CustomBoxImageTextBinding.kt
@@ -3,7 +3,6 @@ package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.cu
 import android.databinding.BindingAdapter
 import android.databinding.InverseBindingMethod
 import android.databinding.InverseBindingMethods
-import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customBoxImage.CustomBoxImage
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customBoxImageText.CustomBoxImageText
 
 @InverseBindingMethods(InverseBindingMethod(type = CustomBoxImageText::class, attribute = "cbit_is_active"))
@@ -11,7 +10,7 @@ class CustomButtonDeactivateListenerBinding {
     companion object {
         @JvmStatic
         @BindingAdapter("cbit_is_active")
-        fun setis_active(mCustomBoxImage: CustomBoxImage?, isActive: Boolean?) {
+        fun setcbit_is_active(mCustomBoxImage: CustomBoxImageText?, isActive: Boolean?) {
             if (mCustomBoxImage != null && isActive != null) {
                 mCustomBoxImage.setActivationState(isActive)
             }
