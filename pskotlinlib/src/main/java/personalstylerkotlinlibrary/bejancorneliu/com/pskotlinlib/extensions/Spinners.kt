5b1caa4a347aade78172e63d9d140d4c96a45998
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-27 09:47:33 +0300

    - CodeReview

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/extensions/Spinners.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/extensions/Spinners.kt
@@ -2,7 +2,9 @@ package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.extensions
 
 import android.widget.Spinner
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customSpinner.adapter.CustomSpinnerAdapter
+import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.databases.Platform
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.model.Item
+import java.util.ArrayList
 
 fun Spinner.setSpinnerEntries(nValues: List<Item>?) {
     if (nValues != null) {
@@ -10,3 +12,12 @@ fun Spinner.setSpinnerEntries(nValues: List<Item>?) {
     }
 }
 
+fun Spinner.setSpinnerEntriesP(nValues: List<Platform>?) {
+    if (nValues != null) {
+        val mList: MutableList<Item> = ArrayList()
+        for (item in nValues) {
+            mList.add(Item(nId = item.id, nValue = item.name))
+        }
+        adapter = CustomSpinnerAdapter(context, mList, "left", false)
+    }
+}
