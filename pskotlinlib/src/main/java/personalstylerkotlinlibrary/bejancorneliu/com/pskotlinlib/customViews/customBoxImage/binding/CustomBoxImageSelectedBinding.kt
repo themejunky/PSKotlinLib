7887d8444605c032f7baade6bb934f1610c00a03
Bejan Corneliu <bejancorneliu@gmail.com>
2018-09-29 23:35:45 +0300

    - CodeReview(3)

--- pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImage/binding/CustomBoxImageSelectedBinding.kt
+++ pskotlinlib/src/main/java/personalstylerkotlinlibrary/bejancorneliu/com/pskotlinlib/customViews/customBoxImage/binding/CustomBoxImageSelectedBinding.kt
@@ -1,23 +1,23 @@
 package personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customBoxImage.binding
 
 import android.databinding.BindingAdapter
+import android.databinding.InverseBindingAdapter
 import android.databinding.InverseBindingMethod
 import android.databinding.InverseBindingMethods
 import android.widget.CompoundButton
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customBoxImage.CustomBoxImage
 import personalstylerkotlinlibrary.bejancorneliu.com.pskotlinlib.customViews.customInput.CustomInput
 
-@InverseBindingMethods(InverseBindingMethod(type = CustomBoxImage::class, attribute = "cbi_is_selected"))
-class CustomBoxImageSelectedBinding {
-    companion object {
-        @JvmStatic
-        @BindingAdapter("cbi_is_selected")
-        fun setcbi_is_selected(nCustomBoxImage: CustomBoxImage?, nState: Boolean?) {
-            if (nCustomBoxImage != null && nState != null) {
-                nCustomBoxImage.setActivationState(nState)
-            }
-        }
+@BindingAdapter("cbi_is_selected")
+fun setcbi_is_selected(nCustomBoxImage: CustomBoxImage?, nState: Boolean?) {
+    if (nCustomBoxImage != null && nState != null) {
+        nCustomBoxImage.setActivationState(nState)
     }
+}
+
+@BindingAdapter("cbi_is_selected")
+fun getcbi_is_selected(box: CustomBoxImage) : Boolean {
+    return  box.getActivationState()
 }
 
 @BindingAdapter("android:checked")
@@ -26,4 +26,3 @@ fun setChecked(nCompoundButton: CompoundButton?, isChecked: Boolean?) {
         nCompoundButton.isChecked = isChecked
     }
 }
-
